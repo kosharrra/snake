4331a6af8f9898ea56b4d3ed05682e20caf22890
Kiska <katisina@mail.ru>
2021-02-07 19:48:57 +0300

    Lesson -13 Полиморфизм (бред)

--- snake/snake/HorizontalLine.cs
+++ snake/snake/HorizontalLine.cs
@@ -18,5 +18,13 @@ namespace Snake
 				pList.Add(p);
 			}
 		}
+		public override void Draw()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+
+			base.Draw();
+
+			Console.ForegroundColor = ConsoleColor.White;
+		}
 	}
 }
