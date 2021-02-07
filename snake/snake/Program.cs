dc357ffdf1c2da36b1728e966b41e53ef7bed4c4
Kiska <katisina@mail.ru>
2021-02-07 16:23:09 +0300

    Lesson-7

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,6 +16,9 @@ namespace Snake
 			Point p2 = new Point(4, 5, '#');
 			p2.Draw();
 
+			HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+			line.Drow();
+
 			Console.ReadLine();
 		}
 	}
