46e06eeaba68eb7d345c51b2a3218123d9a4f8df
Kiska <katisina@mail.ru>
2021-02-07 16:38:54 +0300

    Lesson-8

---
+++ snake/snake/Human.cs
@@ -0,0 +1,14 @@
+﻿/*uusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Human
+	{
+		public int age; // возраст
+	}
+}
+*/
